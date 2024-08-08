8af86dc0620fd4ebd1628feb0ee9e23d5ca5b520
Oleksii Kyslytsa <oleksij.kyslytsja@gmail.com>
2024-08-08 17:17:45 +0300

    chore: Update tasks.json to include Bage, Cnil, and мапу

--- utils/json/json.go
+++ utils/json/json.go
@@ -1,8 +1,10 @@
 package json
 
 import (
-	encoding_json "encoding/json"
+	"fmt"
+	"net/url"
 	"reflect"
+	"sort"
 
 	"github.com/bitly/go-simplejson"
 )
@@ -13,14 +15,63 @@ func NewJSON(data []byte) (j *simplejson.Json, err error) {
 		return nil, err
 	}
 	return j, nil
+} // Функція для перетворення структури в map[string]string формату key=value для подальшого використання в url.Values
+func structToUrlValues(data interface{}, sorted ...bool) (params url.Values, err error) {
+	if len(sorted) == 0 {
+		sorted = append(sorted, true)
+	}
+	params = url.Values{}
+	v := reflect.ValueOf(data)
+
+	// Перевірка, чи є вхідний параметр структурою
+	if v.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("expected a struct, got %s", v.Kind())
+	}
+
+	t := v.Type()
+	fieldMap := url.Values{}
+
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		fieldType := t.Field(i)
+
+		// Пропускаємо пусті поля
+		if isEmptyValue(field) {
+			continue
+		}
+
+		// Додаємо поле у форматі key=value
+		fieldMap.Add(fieldType.Tag.Get("json"), fmt.Sprintf("%v", field.Interface()))
+	}
+
+	if sorted[0] {
+		// Сортуємо ключі
+		keys := make([]string, 0, len(fieldMap))
+		for k := range fieldMap {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+
+		// Додаємо відсортовані ключі до url.Values
+		for _, k := range keys {
+			params.Add(k, fieldMap.Get(k))
+		}
+	} else {
+		// Додаємо несортовані ключі до url.Values
+		params = url.Values(fieldMap)
+	}
+
+	return params, nil
 }
 
-func Marshal(v interface{}) ([]byte, error) {
-	return encoding_json.Marshal(v)
+// Функція для перетворення структури в map[string]string формату key=value з відсортованими ключами
+func StructToSortedUrlValues(data interface{}) (params url.Values, err error) {
+	return structToUrlValues(data, true)
 }
 
-func Unmarshal(data []byte, v interface{}) error {
-	return encoding_json.Unmarshal(data, v)
+// Функція для перетворення структури в map[string]string формату key=value з несортованими ключами
+func StructToUrlValues(data interface{}) (params url.Values, err error) {
+	return structToUrlValues(data, false)
 }
 
 // Функція для перевірки, чи є значення пустим
--- utils/json/json_test.go
+++ utils/json/json_test.go
@@ -1,8 +1,8 @@
+// FILEPATH: /home/oleksii/prj/zero/binance/turbo-restler/utils/json/json_test.go
+
 package json_test
 
 import (
-	encoding_json "encoding/json"
-	"net/url"
 	"testing"
 
 	"github.com/fr0ster/turbo-restler/utils/json"
@@ -13,23 +13,11 @@ func TestNewJSON(t *testing.T) {
 	data := []byte(`{"name": "John", "age": 30}`)
 	j, err := json.NewJSON(data)
 	assert.Nil(t, err)
+	name := j.Get("name").MustString()
+	assert.Equal(t, "John", name)
 
-	// Add assertions here to test the created JSON object
-	assert.Equal(t, "John", j.Get("name").MustString())
-}
-
-func TestStructToQueryString(t *testing.T) {
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-
-	person := Person{Name: "John", Age: 30}
-	queryString, err := json.StructToQueryString(person)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated query string
-	assert.Equal(t, "age=30&name=John", queryString)
+	age := j.Get("age").MustInt()
+	assert.Equal(t, 30, age)
 }
 
 func TestStructToSortedUrlValues(t *testing.T) {
@@ -38,13 +26,17 @@ func TestStructToSortedUrlValues(t *testing.T) {
 		Age  int    `json:"age"`
 	}
 
-	person := Person{Name: "John", Age: 30}
-	params, err := json.StructToSortedUrlValues(person)
+	p := Person{
+		Name: "John",
+		Age:  30,
+	}
+
+	params, err := json.StructToSortedUrlValues(p)
 	assert.Nil(t, err)
 
-	// Add assertions here to test the generated sorted query map
-	assert.Equal(t, "John", params.Get("name"))
-	assert.Equal(t, "age=30&name=John", params.Encode())
+	expected := "age=30&name=John"
+	actual := params.Encode()
+	assert.Equal(t, expected, actual)
 }
 
 func TestStructToUrlValues(t *testing.T) {
@@ -53,55 +45,17 @@ func TestStructToUrlValues(t *testing.T) {
 		Age  int    `json:"age"`
 	}
 
-	person := Person{Name: "John", Age: 30}
-	params, err := json.StructToUrlValues(person)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated query map
-	assert.Equal(t, "John", params.Get("name"))
-	assert.Equal(t, "age=30&name=John", params.Encode())
-}
-
-func TestStructToSortedJSON(t *testing.T) {
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
+	p := Person{
+		Name: "John",
+		Age:  30,
 	}
 
-	person := Person{Name: "John", Age: 30}
-	jsonData, err := json.StructToSortedJSON(person)
+	params, err := json.StructToUrlValues(p)
 	assert.Nil(t, err)
 
-	// Add assertions here to test the generated sorted JSON
-	assert.Equal(t, `{"age":30,"name":"John"}`, string(jsonData))
-}
-
-func TestStructToJSON(t *testing.T) {
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-
-	person := Person{Name: "John", Age: 30}
-	jsonData, err := json.StructToJSON(person)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated JSON
-	assert.Equal(t, `{"age":30,"name":"John"}`, string(jsonData))
-}
-
-func TestStructToSortedSimpleJSON(t *testing.T) {
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-
-	person := Person{Name: "John", Age: 30}
-	jsonData, err := json.StructToSortedSimpleJSON(person)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated sorted simple JSON
-	assert.Equal(t, `{"age":30,"name":"John"}`, jsonData)
+	expected := "age=30&name=John"
+	actual := params.Encode()
+	assert.Equal(t, expected, actual)
 }
 
 func TestIsEmptyValue(t *testing.T) {
@@ -119,129 +73,4 @@ func TestIsEmptyValue(t *testing.T) {
 	assert.Equal(t, "30", params.Get("age"))
 	assert.Equal(t, "", params.Get("params"))
 	assert.Equal(t, "age=30&name=John", params.Encode())
-}
-
-func TestRequest(t *testing.T) {
-	type Request struct {
-		ID     string      `json:"id"`
-		Method string      `json:"method"`
-		Params interface{} `json:"params"`
-	}
-
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-
-	person := Person{Name: "John", Age: 30}
-	_, err := json.StructToSortedUrlValues(person)
-	assert.Nil(t, err)
-
-	request := Request{ID: "1", Method: "test", Params: nil}
-	params, err := json.StructToSortedUrlValues(request)
-	assert.Nil(t, err)
-	jsonParams, err := json.StructToSortedJSON(request)
-	assert.Nil(t, err)
-	simpleJson, err := json.StructToSortedSimpleJSON(request)
-	assert.Nil(t, err)
-	standardJson, err := encoding_json.Marshal(request)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated request
-	assert.Equal(t, "1", params.Get("id"))
-	assert.Equal(t, "test", params.Get("method"))
-	assert.Equal(t, "", params.Get("params"))
-	assert.Equal(t, "id=1&method=test", params.Encode())
-	assert.Equal(t, "{\"id\":\"1\",\"method\":\"test\"}", string(jsonParams))
-	assert.Equal(t, "id=1&method=test", params.Encode())
-	assert.Equal(t, "{\"id\":\"1\",\"method\":\"test\"}", simpleJson)
-	assert.Equal(t, "{\"id\":\"1\",\"method\":\"test\",\"params\":null}", string(standardJson))
-
-	request = Request{ID: "1", Method: "test", Params: person}
-	params, err = json.StructToSortedUrlValues(request)
-	assert.Nil(t, err)
-	jsonParams, err = json.StructToSortedJSON(request)
-	assert.Nil(t, err)
-	simpleJson, err = json.StructToSortedSimpleJSON(request)
-	assert.Nil(t, err)
-	standardJson, err = encoding_json.Marshal(request)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated request
-	assert.Equal(t, "1", params.Get("id"))
-	assert.Equal(t, "test", params.Get("method"))
-	assert.Equal(t, "{John 30}", params.Get("params"))
-	assert.Equal(t, "{\"id\":\"1\",\"method\":\"test\",\"params\":{\"name\":\"John\",\"age\":30}}", string(jsonParams))
-	assert.Equal(t, "id=1&method=test&params=%7BJohn+30%7D", params.Encode())
-	assert.Equal(t, "{\"id\":\"1\",\"method\":\"test\",\"params\":{\"name\":\"John\",\"age\":30}}", simpleJson)
-	assert.Equal(t, "{\"id\":\"1\",\"method\":\"test\",\"params\":{\"name\":\"John\",\"age\":30}}", string(standardJson))
-}
-
-func TestMarshal(t *testing.T) {
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-
-	person := Person{Name: "John", Age: 30}
-	jsonData, err := json.Marshal(person)
-	assert.Nil(t, err)
-	urlValues, err := json.StructToSortedUrlValues(person)
-	assert.Nil(t, err)
-	jsonUrlValues, err := json.UrlValuesToSortedJSON(urlValues)
-	assert.Nil(t, err)
-	byteUrlValues, err := json.UrlValuesToString(urlValues)
-	assert.Nil(t, err)
-	jsonPerson, err := json.StructToSortedJSON(person)
-	assert.Nil(t, err)
-	strPerson, err := json.StructToQueryString(person)
-	assert.Nil(t, err)
-	bytePerson, err := encoding_json.Marshal(person)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated JSON
-	assert.Equal(t, `{"name":"John","age":30}`, string(jsonData))
-	assert.Equal(t, `{"age":30,"name":"John"}`, string(jsonPerson))
-	assert.Equal(t, "age=30&name=John", strPerson)
-	assert.Equal(t, `{"name":"John","age":30}`, string(bytePerson))
-	assert.Equal(t, `{"age":"30","name":"John"}`, string(jsonUrlValues))
-	assert.Equal(t, "age=30&name=John", byteUrlValues)
-}
-
-func StructToSortedQueryByteArr(t *testing.T) {
-	type Person struct {
-		Name string `json:"name"`
-		Age  int    `json:"age"`
-	}
-
-	person := Person{Name: "John", Age: 30}
-	byteArr, err := json.StructToSortedQueryByteArr(person)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated sorted query byte array
-	assert.Equal(t, "age=30&name=John", string(byteArr))
-}
-
-func TestUrlValuesRequest(t *testing.T) {
-	type Request struct {
-		ID     string     `json:"id"`
-		Method string     `json:"method"`
-		Params url.Values `json:"params"`
-	}
-
-	request := Request{ID: "1", Method: "test", Params: nil}
-	params, err := json.StructToSortedUrlValues(request)
-	assert.Nil(t, err)
-	byteRequest, err := encoding_json.Marshal(request)
-	assert.Nil(t, err)
-	strRequest, err := json.StructToSortedQueryByteArr(request)
-	assert.Nil(t, err)
-
-	// Add assertions here to test the generated request
-	assert.Equal(t, "1", params.Get("id"))
-	assert.Equal(t, "test", params.Get("method"))
-	assert.Equal(t, "", params.Get("params"))
-	assert.Equal(t, "id=1&method=test", params.Encode())
-	assert.Equal(t, `{"id":"1","method":"test","params":null}`, string(byteRequest))
-	assert.Equal(t, `{"id":"1","method":"test"}`, string(strRequest))
 }
